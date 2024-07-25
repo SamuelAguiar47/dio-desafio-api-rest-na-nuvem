3cd837dbd02a035584f7246ad6551b6e18b2b81e
SamuelAguiar47 <samuelsaguiar47@gmail.com>
2024-07-25 10:54:09 -0300

    Commit inicial

---
+++ src/main/java/me/dio/DioDesafioApiRestNaNuvemApplication.java
@@ -0,0 +1,14 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioApiRestNaNuvemApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(DioDesafioApiRestNaNuvemApplication.class, args);
+    }
+
+
+}
