e3d105852fdc7ee3e3877aefdb8ea0b546101a47
SamuelAguiar47 <samuelsaguiar47@gmail.com>
2024-07-26 08:06:44 -0300

    Implementação da camada de negócio (Service)

---
+++ src/main/java/me/dio/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package me.dio.service.impl;
+
+import java.util.NoSuchElementException;
+
+import me.dio.domain.model.User;
+import me.dio.domain.repository.UserRepository;
+import me.dio.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public User create(User userToCreate) {
+        //Outra forma de verificação da existência do usuário
+        /*if (userToCreate.getId() != null && userRepository.existsById(userToCreate.getId())) {
+            throw new IllegalArgumentException("This User ID already exists.");
+        }
+        */
+        //Método de verificação da existência do usuário através do número da conta.
+        if (userRepository.existsByAccountNumber(userToCreate.getAccount().getNumber())) {
+            throw new IllegalArgumentException("This Account Number already exists.");
+        }
+        return userRepository.save(userToCreate);
+    }
+    
+}
